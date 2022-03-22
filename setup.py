177ffa8eb32d32119f329bcb639777ba57bc3f83
Gabriel Souza <61873782+az3vedo@users.noreply.github.com>
2022-03-22 08:43:36 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+try:
+    env = sys.argv[1]
+except:
+    env = "production"
+
+os.environ['FLASK_APP']="app"
+os.environ['FLASK_ENV']=env
+os.system('pip install -r ./requirements.txt')
+os.system('flask run --port 5100')
