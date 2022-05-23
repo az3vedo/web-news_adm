5402dc1bfd2cee47e485ed6986c3aaebfb845655
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-05-23 17:40:59 -0300

    fix application port on setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ except:
 os.environ['FLASK_APP']="app"
 os.environ['FLASK_ENV']=env
 os.system('pip install -r ./requirements.txt')
-os.system('flask run --port 5100')
+os.system('flask run --port 5000')
