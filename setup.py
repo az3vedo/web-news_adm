da00c50f55f4070f00bde0a13bd6e56d054a6749
Gabriel Souza <azevedoedit@gmail.com>
2022-06-20 22:31:48 -0300

    add gunicorn library and update setup script

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ except:
 os.environ['FLASK_APP']="app"
 os.environ['FLASK_ENV']=env
 os.system('pip install -r ./requirements.txt')
-os.system('flask run --port 5000')
+os.system('gunicorn wsgi:app -b 0.0.0.0:5000')
