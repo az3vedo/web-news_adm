dc9e31d72b21499a59fe9c1a131d100d266d42d9
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-06-21 10:08:33 -0300

    Fix application CORS and request URL's

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ except:
 os.environ['FLASK_APP']="app"
 os.environ['FLASK_ENV']=env
 os.system('pip install -r ./requirements.txt')
-os.system('gunicorn wsgi:app -b 0.0.0.0:5000')
+os.system('gunicorn wsgi:app -b 127.0.0.1:5000')
